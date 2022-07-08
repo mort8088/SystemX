b2dbf8fc604ece1b6295103902d354d455a3a4a5
Dave Henry <dave@mort8088.com>
2022-07-08 10:50:47 +0100

    Added Mag & Mag2 extesion functions to Vector2

---
+++ Extensions/Vector2Extensions.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Vector2Extensions.cs" company="Mort8088 Games">
+// Copyright (c) 2012-22 Dave Henry for Mort8088 Games.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using Microsoft.Xna.Framework;
+
+namespace SystemX.Extensions {
+
+    /// <summary>
+    ///     Static class for Vector2 extension methods.
+    /// </summary>
+    /// <remarks>
+    ///     kkkk
+    /// </remarks>
+    public static class Vector2Extensions {
+
+        /// <summary>
+        ///     Return the magnitude of a Vector2.
+        /// </summary>
+        public static double Mag(this Vector2 v) {
+            return  Math.Sqrt(v.X * v.X + v.Y * v.Y); 
+        }
+
+        /// <summary>
+        ///     Return the magnitude of a Vector2.
+        /// </summary>
+        public static double Mag2(this Vector2 v) {
+            return (v.X * v.X + v.Y * v.Y);
+        }    
+    }
+}
